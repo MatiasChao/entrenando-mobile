690585ce0f1219f1f965db83dd54ca6573a42942
MatiasChao <matias.chao@despegar.com>
2020-10-17 01:33:08 -0300

    firebase added

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './app/navigations/Navigations';
+import React from 'react'
+import Navigation from './app/navigations/Navigations'
 
 export default function App() {
   return (
