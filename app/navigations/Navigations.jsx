b035241e4b20a88258a17f23936a37844968c045
MatiasChao <matias.chao@despegar.com>
2020-11-07 16:50:47 -0300

    icons added

--- app/navigations/Navigations.jsx
+++ app/navigations/Navigations.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Icon } from 'react-native-elements'
 
 // Stacks
 import ActivitiesStack from '../navigations/ActivitiesStack'
@@ -13,7 +14,16 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+             initialRouteName = "account"
+             tabBarOptions = {{
+                 inactiveTintColor: '#646464',
+                 activeTintColor: '#00a680'
+             }}
+             screenOptions = { ({ route }) => ({
+                 tabBarIcon: ({ color }) => screenOptions(route, color)
+             }) }
+            >
                 <Tab.Screen 
                     name="account" 
                     component={ AccountStack } 
@@ -36,5 +46,30 @@ export default function Navigation() {
                 />
             </Tab.Navigator>
         </NavigationContainer>
+    )
+}
+
+function screenOptions(route, color) {
+    let iconName;
+
+    switch (route.name) {
+        case "account":
+            iconName = "account"
+            break;
+        case "home":
+            iconName = "home"
+            break;
+        case "activities":
+            iconName = "format-list-bulleted"
+            break;
+        case "favorites":
+            iconName = "heart"
+            break;
+        
+        default:
+            break;
+    }
+    return (
+        <Icon type="material-community" name={iconName} size={22} color={color} />
     )
 }
